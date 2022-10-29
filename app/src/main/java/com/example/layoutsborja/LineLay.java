49721f27c8b021de54504b2cd9199ecb3938a991
Borja Chacon <kdl35896@educastur.es>
2022-10-29 19:50:09 +0200

    TAREA3

---
+++ app/src/main/java/com/example/layoutsborja/LineLay.java
@@ -0,0 +1,14 @@
+package com.example.layoutsborja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LineLay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line_lay);
+    }
+}
---
+++ app/src/main/java/com/example/layoutsborja/TablLay.java
@@ -0,0 +1,14 @@
+package com.example.layoutsborja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TablLay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabl_lay);
+    }
+}
